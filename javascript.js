0ddca1c28a48b82593f0bbe9222cea4eb2eb5cca
nk1107 <nishantkumar.nk11072004@gmail.com>
2022-03-19 13:28:21 +0530

    test part complete

--- javascript.js
+++ javascript.js
@@ -1,1 +1,133 @@
-console.log("working")
+function selectionmindset(x){
+    if(x==1){
+        btn=document.getElementById("logical");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==2){
+        btn=document.getElementById("analytical");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==3){
+        btn=document.getElementById("stem");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==4){
+        btn=document.getElementById("disciplined");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==5){
+        btn=document.getElementById("physical");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==6){
+        btn=document.getElementById("reasoning");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==7){
+        btn=document.getElementById("linguistic");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==8){
+        btn=document.getElementById("organisational");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==9){
+        btn=document.getElementById("learning");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    
+}
+
+function selectioninterests(x){
+    if(x==1){
+        btn=document.getElementById("reading");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==2){
+        btn=document.getElementById("travel");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==3){
+        btn=document.getElementById("music");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==4){
+        btn=document.getElementById("art");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==5){
+        btn=document.getElementById("history2");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==6){
+        btn=document.getElementById("space");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==7){
+        btn=document.getElementById("sports");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==8){
+        btn=document.getElementById("movies");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+    else if(x==9){
+        btn=document.getElementById("coding");
+        if(btn.style.backgroundColor != "#0d637e"){
+            btn.style.backgroundColor = "#0d637e";
+            console.log("changed");
+        }
+    }
+
+}
